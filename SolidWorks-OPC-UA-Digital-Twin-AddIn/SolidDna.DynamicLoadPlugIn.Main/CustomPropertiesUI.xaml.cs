859ac759a2cbd1a5b086b7cff14e46b6d889bda9
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2020-03-03 10:23:52 +0100

    Small readability changes

--- SolidWorks-OPC-UA-Digital-Twin-AddIn/SolidDna.DynamicLoadPlugIn.Main/CustomPropertiesUI.xaml.cs
+++ SolidWorks-OPC-UA-Digital-Twin-AddIn/SolidDna.DynamicLoadPlugIn.Main/CustomPropertiesUI.xaml.cs
@@ -13,34 +13,13 @@ namespace SolidDna.DynamicLoadPlugIn
     public partial class CustomPropertiesUI : UserControl
     {
         #region Private Members
-
         private const string CustomPropertyDescription = "Description";
-        private const string CustomPropertyStatus = "Status";
-        private const string CustomPropertyRevision = "Revision";
-        private const string CustomPropertyPartNumber = "PartNo";
-        private const string CustomPropertyManufacturingInformation = "Manufacturing Information";
         private const string CustomPropertyLength = "Length";
-        private const string CustomPropertyFinish = "Finish";
-        private const string CustomPropertyPurchaseInformation = "Purchase Information";
-        private const string CustomPropertySupplierName = "Supplier";
-        private const string CustomPropertySupplierCode = "Supplier Number / Code";
         private const string CustomPropertyNote = "Note";
-
-        private const string ManufacturingWeld = "WELD";
-        private const string ManufacturingAssembly = "ASSEMBLY";
-        private const string ManufacturingPlasma = "PLASMA";
-        private const string ManufacturingLaser = "LASER";
-        private const string ManufacturingPurchase = "PURCHASE";
-        private const string ManufacturingLathe = "LATHE";
-        private const string ManufacturingDrill = "DRILL";
-        private const string ManufacturingFold = "FOLD";
-        private const string ManufacturingRoll = "ROLL";
-        private const string ManufacturingSaw = "SAW";
-
         #endregion
 
-        #region Constructor
 
+        #region Constructor
         /// <summary>
         /// Default constructor
         /// </summary>
@@ -48,44 +27,37 @@ namespace SolidDna.DynamicLoadPlugIn
         {
             InitializeComponent();
         }
-
         #endregion
 
-        #region Startup
 
+        #region Startup
         /// <summary>
         /// Fired when the control is fully loaded
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
         {
             // By default show the No Part open screen
             NoPartContent.Visibility = System.Windows.Visibility.Visible;
-            MainContent.Visibility = System.Windows.Visibility.Hidden;
+            MainContent.Visibility   = System.Windows.Visibility.Hidden;
 
             // Listen out for the active model changing
             Application.ActiveModelInformationChanged += Application_ActiveModelInformationChanged;
         }
-
         #endregion
 
-        #region Model Events
 
+        #region Model Events
         /// <summary>
         /// Fired when the active SolidWorks model is changed
         /// </summary>
-        /// <param name="obj"></param>
         private void Application_ActiveModelInformationChanged(Model obj)
         {
             ReadDetails();
         }
-
         #endregion
 
 
         #region Read Details
-
         /// <summary>
         /// Reads all the details from the active SolidWorks model
         /// </summary>
@@ -102,30 +74,28 @@ namespace SolidDna.DynamicLoadPlugIn
                 {
                     // Show No Part screen
                     NoPartContent.Visibility = System.Windows.Visibility.Visible;
-                    MainContent.Visibility = System.Windows.Visibility.Hidden;
+                    MainContent.Visibility   = System.Windows.Visibility.Hidden;
                     return;
                 }
 
-                // If we got here, we have a part
+                // If we got here, we have a valid model
 
                 // Listen out for selection changes
                 model.SelectionChanged += Model_SelectionChanged;
 
-                // Show the main content
+                // Show the main sidepanel content
                 NoPartContent.Visibility = System.Windows.Visibility.Hidden;
                 MainContent.Visibility   = System.Windows.Visibility.Visible;
 
-                // Get custom properties
                 // Description
                 DescriptionText.Text = model.GetCustomProperty(CustomPropertyDescription);
 
-                // Length
-                SheetMetalLengthText.Text = model.GetCustomProperty(CustomPropertyLength);
+                // Dimension
+                SheetMetalLengthText.Text          = model.GetCustomProperty(CustomPropertyLength);
                 SheetMetalLengthEvaluatedText.Text = model.GetCustomProperty(CustomPropertyLength, resolved: true);
 
-                // Note
+                // Notes
                 NoteText.Text = model.GetCustomProperty(CustomPropertyNote);
-
             });
         }
 
@@ -140,11 +110,10 @@ namespace SolidDna.DynamicLoadPlugIn
                 ThreadHelpers.RunOnUIThread(() => LengthButton.IsEnabled = haveDimension);
             });
         }
-
         #endregion
 
-        #region Set Details
 
+        #region Set Details
         /// <summary>
         /// Sets all the details to the active SolidWorks model
         /// </summary>
@@ -164,17 +133,13 @@ namespace SolidDna.DynamicLoadPlugIn
             // Re-read details to confirm they are correct
             ReadDetails();
         }
-
         #endregion
 
 
         #region Button Events
-
         /// <summary>
         /// Called when the read button is clicked
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void ReadButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             ReadDetails();
@@ -183,8 +148,6 @@ namespace SolidDna.DynamicLoadPlugIn
         /// <summary>
         /// Called when the reset button is clicked
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void ResetButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             // Clear all values
@@ -199,15 +162,14 @@ namespace SolidDna.DynamicLoadPlugIn
         /// <summary>
         /// Called when the apply button is clicked
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void ApplyButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             SetDetails();
         }
-        #endregion
-
-
+        
+        /// <summary>
+        /// Called when 'Get' button is pressed
+        /// </summary>
         private void LengthButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             Application.ActiveModel?.SelectedObjects((objects) =>
@@ -226,5 +188,6 @@ namespace SolidDna.DynamicLoadPlugIn
                 });
             });
         }
+        #endregion
     }
 }
